fdad39580800bc520714f4e72a29b20106dbe21d
David Parker <david.m.parker92@gmail.com>
2023-04-10 13:44:30 -0400

    fixed all work picutes

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,5 +1,11 @@
 import Footer from "../components/footer";
 
+import bookSearch from '../images/bookSearch.png';
+import projectWhatch from "../images/project-whatch.png";
+import techBlog from '../images/techBlog.png';
+import textEditor from '../images/textEditor.png';
+import noteTaker from '../images/noteTaker.png';
+
 function Work(props) {
   return (
     <div>
@@ -13,7 +19,7 @@ function Work(props) {
         <div class="image1">
           <a href="https://booksearch92.herokuapp.com/">
             <img
-              src={require("../images/bookSearch.png")}
+              src={bookSearch}
               class="gridPics"
               alt=""
             />
@@ -23,28 +29,28 @@ function Work(props) {
 
         <div class="image2">
           <a href="https://thelemmonade.github.io/project-whatch/">
-            <img src="./images/project-whatch.png" class="gridPics" alt="" />
+            <img src={projectWhatch} class="gridPics" alt="" />
           </a>
           <h4>Interactive Front End Site</h4>
         </div>
 
         <div class="image3">
           <a href="https://techblog92.herokuapp.com">
-            <img src="./images/techBlog.png" class="gridPics" alt="" />
+            <img src={techBlog} class="gridPics" alt="" />
           </a>
           <h4>Tech Blog</h4>
         </div>
 
         <div class="image4">
           <a href="https://intense-brook-34675.herokuapp.com/">
-            <img src="./images/textEditor.png" class="gridPics" alt="" />
+            <img src={textEditor} class="gridPics" alt="" />
           </a>
           <h4 class="editor">Text Editor</h4>
         </div>
 
         <div class="image5">
           <a href="https://parker92-module-11-challenge.herokuapp.com/">
-            <img src="./images/noteTaker.png" class="gridPics" alt="" />
+            <img src={noteTaker} class="gridPics" alt="" />
           </a>
           <h4>Note Taker</h4>
         </div>
