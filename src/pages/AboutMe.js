f250f5c752ae8bd676839703e7fc0ef97041c825
David Parker <david.m.parker92@gmail.com>
2023-04-12 20:35:49 -0400

    attempted picture add

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -10,13 +10,14 @@ function AboutMe(props) {
             <h3>About Me</h3>
           </aside>
 
+          <img src="./images/headshot.png" class="headshot" alt="" />
           <p>
             Full Stack Web Developer with a background in Education and
             life-long dedication to learning. Proficient in mobile and desktop
             development environments. Adept at using JavaScript and other
             programming languages to produce clean code. Well-organized and
             collaborative team player with strong communication and analytical
-            abilities. I am currently working as a 
+            abilities. I am currently working as a
           </p>
         </section>
       </div>
