26362ccdb507c1569769ebe5cc628bf582eb262c
David Parker <david.m.parker92@gmail.com>
2023-04-17 10:59:29 -0400

    styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 function Home(props) {
   return (
     <div class="HomePage">
-      <div class="home_intro">
+      <div class="home_intro" id="animation">
         <p>I'm David</p>
       </div>
-      <div class="home_2nd">
+      <div class="home_2nd" id="animation">
         <p>A full stack developer</p>
       </div>
     </div>
